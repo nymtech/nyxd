0c26d776bbfb463bdcbafd1f01dd8b11da1b89d6
farbanas <arbanasfran@gmail.com>
2023-04-11 17:25:02 +0200

    add upgrade handler for v0.27.0

--- app/app.go
+++ app/app.go
@@ -419,6 +419,8 @@ func NewWasmApp(
 		app.BaseApp,
 	)
 
+	app.registerUpgradeHandlers()
+
 	// register the staking hooks
 	// NOTE: stakingKeeper above is passed by reference, so that it will contain these hooks
 	app.stakingKeeper = *stakingKeeper.SetHooks(
@@ -907,4 +909,10 @@ func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino
 	paramsKeeper.Subspace(wasm.ModuleName)
 
 	return paramsKeeper
+}
+
+func (app *WasmApp) registerUpgradeHandlers() {
+	app.upgradeKeeper.SetUpgradeHandler("v0.27.0", func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
 }
