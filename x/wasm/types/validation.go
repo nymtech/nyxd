3ca57fd3374a216ce417046de9bfc7c1ca794700
farbanas <arbanasfran@gmail.com>
2023-06-29 16:51:38 +0200

    bumped max wasm size

--- x/wasm/types/validation.go
+++ x/wasm/types/validation.go
@@ -16,10 +16,10 @@ var (
 	MaxLabelSize = 128 // extension point for chains to customize via compile flag.
 
 	// MaxWasmSize is the largest a compiled contract code can be when storing code on chain
-	MaxWasmSize = 800 * 1024 // extension point for chains to customize via compile flag.
+	MaxWasmSize = 2 * 1024 * 1024 // extension point for chains to customize via compile flag.
 
 	// MaxProposalWasmSize is the largest a gov proposal compiled contract code can be when storing code on chain
-	MaxProposalWasmSize = 3 * 1024 * 1024 // extension point for chains to customize via compile flag.
+	MaxProposalWasmSize = 4 * 1024 * 1024 // extension point for chains to customize via compile flag.
 )
 
 func validateWasmCode(s []byte, maxSize int) error {
