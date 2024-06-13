8e6faf4857acdf8acb0bcb76186a73ebc9970e1b
Sachin Kamath <github@skamath.me>
2024-06-13 17:44:01 +0530

    update upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -5,20 +5,6 @@ import (
 
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
-	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
-	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
-	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
-	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
-	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-
-	v2 "github.com/CosmWasm/wasmd/x/wasm/migrations/v2"
-	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	"github.com/nymtech/nyxd/app/upgrades"
 	"github.com/nymtech/nyxd/app/upgrades/noop"
 	v051 "github.com/nymtech/nyxd/app/upgrades/v051"
@@ -29,7 +15,6 @@ var Upgrades = []upgrades.Upgrade{v051.Upgrade}
 
 // RegisterUpgradeHandlers registers the chain upgrade handlers
 func (app *WasmApp) RegisterUpgradeHandlers() {
-	setupLegacyKeyTables(&app.ParamsKeeper)
 	if len(Upgrades) == 0 {
 		// always have a unique upgrade registered for the current version to test in system tests
 		Upgrades = append(Upgrades, noop.NewUpgrade(app.Version()))
@@ -73,40 +58,4 @@ func (app *WasmApp) RegisterUpgradeHandlers() {
 			break
 		}
 	}
-}
-
-func setupLegacyKeyTables(k *paramskeeper.Keeper) {
-	for _, subspace := range k.GetSubspaces() {
-		subspace := subspace
-
-		var keyTable paramstypes.KeyTable
-		switch subspace.Name() {
-		case authtypes.ModuleName:
-			keyTable = authtypes.ParamKeyTable() //nolint:staticcheck
-		case banktypes.ModuleName:
-			keyTable = banktypes.ParamKeyTable() //nolint:staticcheck
-		case stakingtypes.ModuleName:
-			keyTable = stakingtypes.ParamKeyTable() //nolint:staticcheck
-		case minttypes.ModuleName:
-			keyTable = minttypes.ParamKeyTable() //nolint:staticcheck
-		case distrtypes.ModuleName:
-			keyTable = distrtypes.ParamKeyTable() //nolint:staticcheck
-		case slashingtypes.ModuleName:
-			keyTable = slashingtypes.ParamKeyTable() //nolint:staticcheck
-		case govtypes.ModuleName:
-			keyTable = govv1.ParamKeyTable() //nolint:staticcheck
-		case crisistypes.ModuleName:
-			keyTable = crisistypes.ParamKeyTable() //nolint:staticcheck
-			// wasm
-		case wasmtypes.ModuleName:
-			keyTable = v2.ParamKeyTable() //nolint:staticcheck
-		default:
-			continue
-		}
-
-		if !subspace.HasKeyTable() {
-			subspace.WithKeyTable(keyTable)
-		}
-	}
-
 }
--- app/upgrades/v051/upgrades.go
+++ app/upgrades/v051/upgrades.go
@@ -2,18 +2,19 @@ package v050
 
 import (
 	"context"
+	"fmt"
 
 	storetypes "cosmossdk.io/store/types"
 	circuittypes "cosmossdk.io/x/circuit/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
-
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 
 	"github.com/nymtech/nyxd/app/upgrades"
 )
 
 // UpgradeName defines the on-chain upgrade name
-const UpgradeName = "v0.50"
+const UpgradeName = "v0.51"
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
@@ -33,6 +34,23 @@ func CreateUpgradeHandler(
 ) upgradetypes.UpgradeHandler {
 	// sdk 47 to sdk 50
 	return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return mm.RunMigrations(ctx, configurator, fromVM)
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+		logger := sdkCtx.Logger().With("upgrade", UpgradeName)
+
+		logger.Info(fmt.Sprintf("== Starting %s Upgrade... ==", UpgradeName))
+		logger.Info(fmt.Sprintf("Pre-upgrade map: %v", fromVM))
+		logger.Info("== Running any configured module migrations == ")
+		newVersionMap, err := mm.RunMigrations(ctx, configurator, fromVM)
+		if err != nil {
+			return nil, err
+		}
+		logger.Info("== Module migrations complete == ")
+		logger.Info(fmt.Sprintf("Post-upgrade map: %v", newVersionMap))
+		logger.Info("== Upgrade complete! == ")
+
+		// TODO : Consider asserting invariants
+
+		return newVersionMap, err
+
 	}
 }
